7ffe0f31d5697baf84f7ccb74db066a541122123
Yang Jun <jun.yang@target-energysolutions.com>
2019-12-10 23:39:38 +0800

    handle texture update

--- packages/plotly-image3d/src/convert.ts
+++ packages/plotly-image3d/src/convert.ts
@@ -11,7 +11,6 @@ class Image3DTrace {
   }
 
   update(data: Data){
-    this.data = data
     this.data.hoverinfo = "skip"
 
     const { x, y, z } = data
@@ -20,16 +19,25 @@ class Image3DTrace {
     const p2: Position = [x[1] * sx, y[1] * sy, z[1] * sz]
     const p3: Position = [x[2] * sx, y[2] * sy, z[2] * sz]
     const p4: Position = [x[3] * sx, y[3] * sy, z[3] * sz]
+    if(data.texture?.src!==this.data.texture?.src && data.texture) {
+      data.texture.addEventListener('load',()=>{
+        if(data.texture.src === this.data.texture.src) {
+          this.plane.update({
+            texture: data.texture
+          })
+          this.scene.glplot.redraw()
+        }
+      })
+    }
     this.plane.update({
       points:[p1,p2,p3,p4],
       opacity: data.opacity
     })
+    this.data = data
   }
   handlePick(){
-    console.log("handlePick")
   }
   dispose() {
-    console.log("dispose")
     this.scene.glplot.remove(this.plane)
   }
 }
